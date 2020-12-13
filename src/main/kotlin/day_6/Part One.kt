591e9a2f39c8ba6496d1316022aac2e91128c96b
Infinidrix <biruksolomon11@gmail.com>
2020-12-13 15:06:17 +0300

    Add day six

---
+++ src/main/kotlin/day_6/Part One.kt
@@ -0,0 +1,38 @@
+package day_6
+
+import java.nio.file.Path
+
+fun main(args: Array<String>){
+    var filename = "input1.txt"
+    if (args.isNotEmpty()){
+        filename = args[0]
+    }
+
+    val choices = parseFile(filename)
+    var count = 0
+    for (choiceList in choices){
+        val charCount = Array<Int>(26) {0}
+        choiceList.forEach {
+            charCount[it.toInt() - 'a'.toInt()] = 1
+        }
+        count += charCount.sum()
+    }
+
+    println("The sum of yes counts is $count")
+}
+
+fun parseFile(filename: String): List<String> {
+    val output = mutableListOf<String>()
+
+    val path = Path.of("src", "main", "kotlin", "day_6", filename).toAbsolutePath()
+    val temp = mutableListOf<String>()
+    path.toFile().bufferedReader().lines().forEach { line ->
+        if (line.isEmpty() || line.isBlank()){
+            output.add(temp.joinToString(""))
+            temp.clear()
+        }else{
+            temp.add(line)
+        }
+    }
+    return output
+}
---
+++ src/main/kotlin/day_6/Part Two.kt
@@ -0,0 +1,33 @@
+package day_6
+
+import java.nio.file.Path
+
+fun main(args: Array<String>){
+    var filename = "input1.txt"
+    if (args.isNotEmpty()){
+        filename = args[0]
+    }
+
+    println("The sum of all yes counts is ${parseFileWithSet(filename)}")
+}
+
+fun parseFileWithSet(filename: String): Int {
+    var output = 0
+
+    val path = Path.of("src", "main", "kotlin", "day_6", filename).toAbsolutePath()
+    var temp = mutableSetOf<Char>()
+    var first = true
+    path.toFile().bufferedReader().lines().forEach { line ->
+        if (line.isEmpty() || line.isBlank()) {
+            output += temp.size
+            first = true; temp = mutableSetOf()
+        } else if (first){
+            temp.addAll(line.toSet())
+            first = false
+        }else{
+            val tempSet = line.toSet()
+            temp.removeIf {!tempSet.contains(it)}
+        }
+    }
+    return output
+}
