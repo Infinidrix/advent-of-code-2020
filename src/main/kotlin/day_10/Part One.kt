a77182531661b47086fe8f2515846924157625da
Infinidrix <biruksolomon11@gmail.com>
2020-12-16 09:07:55 +0300

    Add day ten

---
+++ src/main/kotlin/day_10/Part One.kt
@@ -0,0 +1,35 @@
+package day_10
+
+import java.nio.file.Path
+
+
+fun main(args: Array<String>){
+    val filename = if (args.isNotEmpty()) args[0] else "input1.txt"
+
+    val adapters = parseFile(filename).toSet()
+    val countArray = Array(3) { 0 }
+    var countConnected = 0
+    var jolts = 0
+    while (countConnected < adapters.size){
+        for (i in 1..3){
+            if (jolts + i in adapters){
+                countArray[i - 1]++
+                jolts += i
+                countConnected++
+                break
+            }
+        }
+    }
+    countArray[2]++ // plus one because the device joltage is always 3 above the highest joltage
+    println("The number of 1 jolt diffs is ${countArray[0]} and 3 jolt diffs is ${countArray[2]} \n" +
+            "making the product ${countArray[0] * countArray[2]}")
+
+}
+fun parseFile(filename: String): Array<Int> {
+    val output = arrayListOf<Int>()
+
+    val path = Path.of("src", "main", "kotlin", "day_10", filename).toAbsolutePath()
+    path.toFile().bufferedReader().lines().forEach { output.add(it.toInt()) }
+
+    return output.toTypedArray()
+}
---
+++ src/main/kotlin/day_10/Part Two.kt
@@ -0,0 +1,22 @@
+package day_10
+
+fun main(args: Array<String>){
+    val filename = if (args.isNotEmpty()) args[0] else "input1.txt"
+
+    val adapters = arrayListOf(0)
+    adapters.addAll(parseFile(filename).sortedArray())
+    val possibleArrangements = Array(adapters.size) {0L}
+    possibleArrangements[adapters.size - 1] = 1L
+    for (index in adapters.size-2 downTo 0){
+        val currVal = adapters[index]
+        var nextInd = index + 1
+        var nextVal = adapters[nextInd]
+        while (nextVal in currVal+1..currVal+3){
+            possibleArrangements[index] += possibleArrangements[nextInd]
+            if (nextInd == adapters.size - 1) break
+            nextVal = adapters[++nextInd]
+        }
+    }
+    println("There are ${possibleArrangements[0]} ways to arrange adapters")
+
+}
